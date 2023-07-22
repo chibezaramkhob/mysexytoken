87c10a27e7a8dba4f725c0809191b0c2e7a0bdaf
chibezaramkhob <111318707+chibezaramkhob@users.noreply.github.com>
2023-07-22 10:53:34 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x4bcc192a68f0b4e48b863211a0c1bb396525b9ff";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
